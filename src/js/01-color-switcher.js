ee479b3b444a26fb43beb98638d113f81b77d511
Lilianademo <tataruliliana343@gmail.com>
2023-12-01 16:52:31 +0200

    homework-js-9

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,23 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+const startButton = document.querySelector("[data-start]");
+const stopButton = document.querySelector("[data-stop]");
+let timerId = null;
+startButton.addEventListener("click", () => {
+  if (timerId !== null) {
+    return;
+  }
+  timerId = setInterval(setBodyBackground, 1000);
+});
+stopButton.addEventListener("click", () => {
+  if (timerId !== null) {
+    clearInterval(timerId);
+  }
+});
+function setBodyBackground() {
+  const randomCollor = getRandomHexColor();
+  document.body.style = `background-color: ${randomCollor}`;
+}
---
+++ src/js/03-promises.js
@@ -0,0 +1,43 @@
+import Notiflix from "notiflix";
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+const delayInput = document.getElementsByName("delay");
+const stepInput = document.getElementsByName("step");
+const amountInput = document.getElementsByName("amount");
+
+const form = document.querySelector(".form");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  let delay = Number(delayInput[0].value);
+  const step = Number(stepInput[0].value);
+  const amount = Number(amountInput[0].value);
+  for (let i = 0; i < amount; i++) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `:white_check_mark: Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `:x: Rejected promise ${position} in ${delay}ms`
+        );
+      });
+      
+    delay = delay + step;
+  }
+});
